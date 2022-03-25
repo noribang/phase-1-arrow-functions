6a76255283cd1088ca764d7744aaac62df09bd7f
hoonkim <hoonk9387@gmail.com>
2022-03-24 19:48:25 -0500

    Completed assignment.

---
+++ index.js
@@ -0,0 +1,15 @@
+// 1) has a function expression called divide
+// 2) divide divides 2000 by 100
+const divide = function() {
+    return 2000/100
+}
+// 3) has an arrow function called square
+// 4) square arrow function takes one parameter and multiplies it times itself
+/* This works. Arrow function Basic syntax. */
+// const square = (par1) => {return par1 * par1}
+/* Simpler, cleaner syntax. One parameter and on expression with implicit return. */
+const square = par1 => par1 * par1
+// 5) has an arrow function called add
+// 6) add arrow function takes two parameters and adds them together
+const add = (par1, par2) => par1 + par2 
+
